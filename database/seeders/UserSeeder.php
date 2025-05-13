61183feecaa8183ac42edaad7874a8848142d167
Pepe <se.pepemartin@gmail.com>
2025-05-13 09:14:05 +0200

    Welcome

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,43 +14,58 @@ class UserSeeder extends Seeder
         $admin = User::create([
             'name' => 'Admin User',
             'email' => 'admin@example.com',
-            'password' => Hash::make('Admin.123'),
+            'password' => Hash::make('Site.123'),
         ]);
         $admin->assignRole('admin');
 
         $gestor = User::create([
             'name' => 'Gestor User',
             'email' => 'gestor@example.com',
-            'password' => Hash::make('Admin.123'),
+            'password' => Hash::make('Site.123'),
         ]);
         $gestor->assignRole('gestor');
 
         $editor1 = User::create([
             'name' => 'Editor Uno',
             'email' => 'editor1@example.com',
-            'password' => Hash::make('Admin.123'),
+            'password' => Hash::make('Site.123'),
         ]);
         $editor1->assignRole('editor');
 
         $editor2 = User::create([
             'name' => 'Editor Dos',
             'email' => 'editor2@example.com',
-            'password' => Hash::make('Admin.123'),
+            'password' => Hash::make('Site.123'),
         ]);
         $editor2->assignRole('editor');
 
         $user1 = User::create([
-            'name' => 'Usuario Uno',
+            'name' => 'User Uno',
             'email' => 'user1@example.com',
-            'password' => Hash::make('Admin.123'),
+            'password' => Hash::make('Site.123'),
         ]);
         $user1->assignRole('user');
 
         $user2 = User::create([
-            'name' => 'Usuario Dos',
+            'name' => 'User Dos',
             'email' => 'user2@example.com',
-            'password' => Hash::make('Admin.123'),
+            'password' => Hash::make('Site.123'),
         ]);
         $user2->assignRole('user');
+
+        $invited1 = User::create([
+            'name' => 'Invited Uno',
+            'email' => 'invited1@example.com',
+            'password' => Hash::make('Site.123'),
+        ]);
+        $invited1->assignRole('invited');
+
+        $invited2 = User::create([
+            'name' => 'Invited Dos',
+            'email' => 'invited2@example.com',
+            'password' => Hash::make('Site.123'),
+        ]);
+        // $invited1->assignRole('invited');  sense rol
+
     }
 }
